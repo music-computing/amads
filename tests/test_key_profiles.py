a7580436793de686c0f42f8fec30160cab1c6fde
Huw Cheston <hwc31@cam.ac.uk>
2025-03-27 14:03:30 +0000

    Pytest, not unittest

--- tests/test_key_profiles.py
+++ tests/test_key_profiles.py
@@ -1,60 +1,57 @@
 """Test suite for key profiles in `pitch.key.profiles.py`"""
 
-import unittest
+import pytest
 
 from amads.pitch.key.profiles import source_list
 
 
-class KeyProfileTest(unittest.TestCase):
-    def test_string_attributes(self):
-        """Every key profile should define `name`, `literature`, and `about` attributes as (non-empty) strings"""
-        expected_attrs = ["name", "literature", "about"]
-        for profile in source_list:
-            # This dunder method should just be set to the name of the profile
-            self.assertTrue(
-                str(profile()) == profile().__str__() == getattr(profile, "name")
-            )
-            # Test that the name of the class is the same as its .name attribute
-            self.assertTrue(profile().__class__.__name__ == getattr(profile, "name"))
-            for attr in expected_attrs:
-                # The class should have the attribute
-                self.assertTrue(hasattr(profile, attr))
-                # The attribute should be a string type
-                self.assertIsInstance(getattr(profile, attr), str)
-                # The attribute should not be an empty string
-                self.assertTrue(getattr(profile, attr) != "")
+def test_string_attributes():
+    """Every key profile should define `name`, `literature`, and `about` attributes as (non-empty) strings"""
+    expected_attrs = ["name", "literature", "about"]
+    for profile in source_list:
+        # This dunder method should just be set to the name of the profile
+        assert str(profile()) == profile().__str__() == getattr(profile, "name")
+        # The name of the class should be the same as its .name attribute
+        assert profile().__class__.__name__ == getattr(profile, "name")
+        # The class should have all the desired attributes as non-empty strings
+        for attr in expected_attrs:
+            assert hasattr(profile, attr)
+            assert isinstance(getattr(profile, attr), str)
+            assert getattr(profile, attr) != ""
 
-    def test_array_attributes(self):
-        """Check list attributes provided for each class"""
-        for profile in source_list:
-            # Iterate over every attribute for this profile
-            for attr_key in profile.__dict__.keys():
-                # Skip over this attribute, which gives us a tuple of all defined attributes
-                if attr_key == "__match_args__":
-                    continue
-                # Get the values of the attribute
-                attr_val = getattr(profile, attr_key)
-                # If the attribute is a list
-                if isinstance(attr_val, tuple):
-                    # Every element should be a floating point number
-                    for element in attr_val:
-                        self.assertIsInstance(element, float)
-                    self.assertEqual(len(attr_val), 12)
 
-    def test_sum_attributes(self):
-        """If we provide a `_sum` attribute, this should be normalised to sum to 1."""
-        # Iterate over all the key profiles we've defined
-        for profile in source_list:
-            # Iterate over all the attributes in this class
-            for attr in profile.__dict__.keys():
-                # If we've defined an attribute ending with `_sum`
-                if attr.endswith("_sum"):
-                    # We'd expect the sum of this attribute to be approximately equal to 1.
-                    summed = sum(getattr(profile, attr))
-                    self.assertAlmostEquals(summed, 1.0, places=2)
+def test_array_attributes():
+    """Check list attributes provided for each class"""
+    for profile in source_list:
+        # Iterate over every attribute for this profile
+        for attr_key in profile.__dict__.keys():
+            # Skip over this attribute, which gives us a tuple
+            if attr_key == "__match_args__":
+                continue
+            # Get the values of attribute
+            attr_val = getattr(profile, attr_key)
+            # Every element of tuples should be a floating point number
+            #  We should have 12 of them, one per key
+            if isinstance(attr_val, tuple):
+                assert all(isinstance(element, float) for element in attr_val)
+                assert len(attr_val) == 12
 
-    def test_missing_attributes(self):
-        """Test that we raise errors properly when trying to access missing attributes"""
-        for profile in source_list:
-            with self.assertRaises(AttributeError):
-                _ = profile().__getitem__("missing")
+
+def test_sum_attributes():
+    """If we provide a `_sum` attribute, this should be normalised to sum to 1."""
+    # Iterate over all the key profiles we've defined
+    for profile in source_list:
+        # Iterate over all the attributes in this class
+        for attr in profile.__dict__.keys():
+            # If we've defined an attribute ending with `_sum`
+            if attr.endswith("_sum"):
+                # We'd expect the sum of this attribute to approximately equal 1.
+                summed = sum(getattr(profile, attr))
+                assert pytest.approx(summed, rel=1e-2) == 1.0
+
+
+def test_missing_attributes():
+    """Test that we raise errors properly when trying to access missing attributes"""
+    for profile in source_list:
+        with pytest.raises(AttributeError):
+            _ = profile().__getitem__("missing")
