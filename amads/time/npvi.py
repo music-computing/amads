22d4f940a7e592c4c55e3a473e664e021ae3c03f
Huw Cheston <hwc31@cam.ac.uk>
2025-03-27 18:48:24 +0000

    npvi docstring should no longer crash sphinx (still looks bad though)

--- amads/time/npvi.py
+++ amads/time/npvi.py
@@ -15,41 +15,42 @@ Citation: Daniele, J. R., & Patel, A. D. (2013). An Empirical Study of Historica
 from typing import Iterable
 
 
-def normalized_pairwise_variability_index(durations: Iterable[float]) -> float:
+# flake8: noqa: W605
+def normalized_pairwise_variability_index(
+    durations: Iterable[float],
+) -> float:
     """
     Extracts the normalised pairwise variability index (nPVI).
 
     The nPVI is a measure of variability between successive elements in a sequence, commonly used
-    in music analysis to quantify rhythmic variability. The equation is
-    .. math:: \text{nPVI} = \frac{100}{m-1} \times \sum\limits_{k=1}^{m-1} \left| \frac{d_k - d_{k+1}}{\frac{d_k + d_{k+1}}{2}} \right|    # noqa: W605
-       :label: nPVI
-    where :math:`n` is the number of intervals, and :math:`d_i` is the duration of the :math:`i^{th}` interval.
+    in music analysis to quantify rhythmic variability. The equation is:
 
-    Parameters
-    ----------
-    durations : Iterable[float]
-        The durations to analyse
+    .. math::
 
-    Returns
-    -------
-    float
-        The extracted nPVI value.
+       \text{nPVI} = \frac{100}{m-1} \times \sum\limits_{k=1}^{m-1}
+       \left| \frac{d_k - d_{k+1}}{\frac{d_k + d_{k+1}}{2}} \right|
 
-    Examples
-    --------
-    The first example is taken from Condit-Schultz (2019, Figure 1).
+    where :math:`m` is the number of intervals, and :math:`d_k` is the duration of the :math:`k^{th}` interval.
+
+    Args:
+        durations (Iterable[float]): the durations to analyse
+
+    Returns:
+        float: the extracted nPVI value.
+
+    Examples:
+        The first example is taken from Condit-Schultz (2019, Figure 1).
 
         >>> durs = [1., 1., 1., 1.]
         >>> normalized_pairwise_variability_index(durations=durs)
         0.
 
-    The second example is Daniele & Patel (2013, Appendix).
+        The second example is Daniele & Patel (2013, Appendix).
 
         >>> durs = [1., 1/2, 1/2, 1., 1/2, 1/2, 1/3, 1/3, 1/3, 2., 1/3, 1/3, 1/3, 1/3, 1/3, 1/3, 3/2, 1., 1/2]
         >>> x = normalized_pairwise_variability_index(durations=durs)
         >>> round(x, 1)
         42.2
-
     """
 
     # Explicitly check to make sure we have enough elements to calculate nPVI
