b5b394b7da63da2a5c49c57088d28de0812433c8
Mark Gotham <19486660+MarkGotham@users.noreply.github.com>
2025-04-05 13:41:42 +0100

    Update break_it_up.py
    ... in line with new meter encoding

--- amads/time/meter/break_it_up.py
+++ amads/time/meter/break_it_up.py
@@ -15,103 +15,114 @@ within-measure notational conventions).
 __author__ = "Mark Gotham"
 
 
-from . import MetricalHierarchy
-
 # ------------------------------------------------------------------------------
 
 
 class MetricalSplitter:
     """
-        Split up notes and rest to reflect a specified metrical hierarchies.
+    Split up notes and rest to reflect a specified metrical hierarchies.
 
-        This class
-        takes in a representation of a note in terms of the start position and duration,
-        along with a metrical context
-        and returns a list of start-duration pairs for the broken-up note value.
+    This class
+    takes in a representation of a note in terms of the start position and duration,
+    along with a metrical context
+    and returns a list of start-duration pairs for the broken-up note value.
 
-        The metrical context should be expressed in the form of a `q
-        (effectively a list of lists for the hierarchy).
-        This can be provided directly, or made via the `MetricalHierarchy` class.
-        See notes on that class for the creation of hierarchies suitable to the task at hand
-        via a range of entry point.
+    The metrical context should be expressed in the form of a `start_hierarchy`
+    (effectively a list of lists for the hierarchy).
+    This can be provided directly or made via various classes in the meter module (see notes there).
 
-        The basic premise here is that a single note can only traverse metrical boundaries
-        for levels lower than the one it starts on.
-        If it traverses the metrical boundary of a higher level, then
-        it is split at that position into two note-heads.
-        This split registers as a syncopation for those algorithms
-        and as a case for two note-heads to be connected by a tie in notation.
+    The basic premise here is that a single note can only traverse metrical boundaries
+    for levels lower than the one it starts on.
+    If it traverses the metrical boundary of a higher level, then
+    it is split at that position into two note-heads.
+    This split registers as a case of syncopation for those algorithms
+    and as a case for two note-heads to be connected by a tie in notation.
 
-        There are many variants on this basic set up.
-        This class aims to support almost any such variant, while providing easy defaults
-        for simple, standard practice.
+    There are many variants on this basic setup.
+    This class aims to support almost any such variant, while providing easy defaults
+    for simple, standard practice.
 
-        The flexibility comes from the definition of a metrical structure
-        (for which see the `MetricalHierarchy` class).
+    The flexibility comes from the definition of a metrical structure
+    (for which see the `MetricalHierarchy` class).
 
-        Each split of the note duration serves to move up one metrical level.
-        For instance, for the 4/4 example above, a note of duration 2.0 starting at start
-        0.25 connects to 0.5 in level 3 (duration = 0.25), then
-        0.5 connects to 1.0 in level 2 (duration = 0.5), then
-        1.0 connects to 2.0 in level 1 (duration = 1.0), and
-        this leaves a duration of 0.25 to start on start 2.0.
-        The data is returned as a list of (position, duration) tuples.
-        The values for the example would be:
-        [(0.25, 0.25),
-        (0.5, 0.5),
-        (1.0, 1.0),
-        (1.0, 0.25)]
-        as demonstrated at ** below.
+    Each split of the note duration serves to move up one metrical level.
+    For instance, for the 4/4 example above, a note of duration 2.0 starting at start
+    0.25 connects to 0.5 in level 3 (duration = 0.25), then
+    0.5 connects to 1.0 in level 2 (duration = 0.5), then
+    1.0 connects to 2.0 in level 1 (duration = 1.0), and
+    this leaves a duration of 0.25 to start on start 2.0.
+    The data is returned as a list of (position, duration) tuples.
+    The values for the example would be:
+    [(0.25, 0.25),
+    (0.5, 0.5),
+    (1.0, 1.0),
+    (1.0, 0.25)]
+    as demonstrated below.
 
-        Parameters
-        -------
-        note_start: float
-            The starting position of the note (or rest).
+    If the note runs past the end of the metrical span,
+    the remaining value is stored with the
+    `start_duration_pairs` recording the within-measure pairs
+    and `remaining_length` attribute for the rest.
 
-        note_length: float,
-            The length (duration) of the note (or rest).
+    If the `note_start` is not in the hierarchy,
+    then the fist step is to map to the next nearest value in the lowest level.
 
-        meter: MetricalHierarchy,
-            The metrical strcuture to compare.
+    Parameters
+    -------
+    note_start: float
+        The starting position of the note (or rest).
 
-        split_same_level: bool
-            When creating hierarchies, decide whether to split elements at the same level, e.g., 1/8 and 2/8 in 6/8.
-            In cases of metrical structures with a 3-grouping
-            (two "weak" events between a "strong" in compound signatures like 6/8)
-            some conventions chose to split notes within-level as well as between them.
-            For instance, with a quarter note starting on the second eighth note (start 0.5) of 6/8,
-            some will want to split that into two 1/8th notes, divided on the third eighth note position,
-            while others will want to leave this intact.
-            The `split_same_level` option accommodates this:
-            it effects the within-level split when set to True and not otherwise (default).
+    note_length: float,
+        The length (duration) of the note (or rest).
 
-        Examples
-        -------
-        >>> m = MetricalHierarchy("4/4")
-        >>> split = MetricalSplitter(0.25, 2.0, meter=m)
-        >>> split.start_duration_pairs
-        [(0.25, 0.25), (0.5, 0.5), (1.0, 1.0), (2.0, 0.25)]
+    split_same_level: bool
+        When creating hierarchies, decide whether to split elements at the same level, e.g., 1/8 and 2/8 in 6/8.
+        In cases of metrical structures with a 3-grouping
+        (two "weak" events between a "strong" in compound signatures like 6/8),
+        some conventions chose to split notes within-level as well as between them.
+        For instance, with a quarter note starting on the second eighth note (start 0.5) of 6/8,
+        some will want to split that into two 1/8th notes, divided on the third eighth note position,
+        while others will want to leave this intact.
+        The `split_same_level` option accommodates this:
+        it affects the within-level split when set to True and not otherwise (default).
 
-        If the note runs past the end of the metrical span,
-        the remaining value is stored with the
-        `start_duration_pairs` recording the within-measure pairs
-        and `remaining_length` attribute for the rest.
+    Examples
+    -------
 
-        >>> split = MetricalSplitter(0.25, 4.0, meter=m)
-        >>> split.start_duration_pairs
-        [(0.25, 0.25), (0.5, 0.5), (1.0, 1.0), (2.0, 2.0)]
+    >>> from amads.time.meter import TimeSignature, PulseLengths
+    >>> m = TimeSignature(as_string="4/4")
+    >>> start_hierarchy = m.to_start_hierarchy()
+    >>> start_hierarchy
+    [[0.0, 4.0], [0.0, 1.0, 2.0, 3.0, 4.0]]
 
-        >>> split.remaining_length
-        0.25
+    >>> split = MetricalSplitter(0.25, 2.0, start_hierarchy=start_hierarchy, split_same_level=False)
+    >>> split.start_duration_pairs
+    [(0.25, 0.75), (1.0, 1.25)]
 
-        If the `note_start` is not in the hierarchy,
-        then the fist step is to map to the next nearest value in the lowest level.
+    >>> split = MetricalSplitter(0.25, 2.0, start_hierarchy=start_hierarchy, split_same_level=True)
+    >>> split.start_duration_pairs
+    [(0.25, 0.75), (1.0, 1.0), (2.0, 0.25)]
 
-        >>> split = MetricalSplitter(0.05, 2.0, meter=m)
-        >>> split.start_duration_pairs
-        [(0.05, 0.0125), (0.0625, 0.0625), (0.125, 0.125), (0.25, 0.25), (0.5, 0.5), (1.0, 1.0), (2.0, 0.05)]
+    >>> m.convert_to_2s_3s()
+    >>> start_hierarchy = m.to_start_hierarchy()
+    >>> start_hierarchy
+    [[0.0, 4.0], [0.0, 2.0, 4.0], [0.0, 1.0, 2.0, 3.0, 4.0]]
 
-    )
+    >>> meter_from_pulses = PulseLengths([4, 2, 1, 0.5, 0.25], measure_length=4)
+    >>> start_hierarchy = meter_from_pulses.to_start_hierarchy()
+    >>> start_hierarchy[-1]
+    [0.0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0, 3.25, 3.5, 3.75, 4.0]
+
+    >>> split = MetricalSplitter(0.25, 4.0, start_hierarchy=start_hierarchy)
+    >>> split.start_duration_pairs
+    [(0.25, 0.25), (0.5, 0.5), (1.0, 1.0), (2.0, 2.0)]
+
+    >>> split.remaining_length
+    0.25
+
+    >>> split = MetricalSplitter(0.05, 2.0, start_hierarchy=start_hierarchy)
+    >>> split.start_duration_pairs
+    [(0.05, 0.2), (0.25, 0.25), (0.5, 0.5), (1.0, 1.0), (2.0, 0.05)]
 
     """
 
@@ -119,14 +130,13 @@ class MetricalSplitter:
         self,
         note_start: float,
         note_length: float,
-        meter: MetricalHierarchy,
+        start_hierarchy: list[list],
         split_same_level: bool = True,
     ):
 
         self.note_length = note_length
         self.note_start = note_start
-        self.meter = meter
-        self.start_hierarchy = meter.start_hierarchy
+        self.start_hierarchy = start_hierarchy
         self.split_same_level = split_same_level
 
         # Initialise
@@ -196,7 +206,7 @@ class MetricalSplitter:
                     self.start_duration_pairs.append(
                         (self.updated_start, round(self.remaining_length, 4))
                     )
-                    # done but still reduce remaining_length to end the whole process in level_pass
+                    # done but still reduce `remaining_length` to end the whole process in level_pass
                     self.remaining_length -= duration_to_next_position
                     return
                 else:  # self.remaining_length > duration_to_next_position:
