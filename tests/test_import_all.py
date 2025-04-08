3f0e2f3d123088da5a5ded05d62a478641e60c77
Peter Harrison <pmch2@cam.ac.uk>
2025-04-08 21:21:14 +0100

    Fix test

--- tests/test_import_all.py
+++ tests/test_import_all.py
@@ -1,6 +1,6 @@
 def test_get_root_parncutt_1988():
-    from amads.all import get_root_parncutt_1988
+    from amads.all import ParncuttRootAnalysis
 
     chord = [0, 4, 7]
-    root = get_root_parncutt_1988(chord)
-    assert root == 0
+    analysis = ParncuttRootAnalysis(chord)
+    assert analysis.root == 0
