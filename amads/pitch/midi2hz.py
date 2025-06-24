e681c00ba201714c52c2965fd2c4f9d4d2e1283f
Isaac Li <isaacli@andrew.cmu.edu>
2025-06-24 17:43:55 -0400

    Moved out of a nested function and renamed to _convert_midi_to_hz()

--- amads/pitch/midi2hz.py
+++ amads/pitch/midi2hz.py
@@ -5,28 +5,7 @@ Original doc: https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&
               doi=6e06906ca1ba0bf0ac8f2cb1a929f3be95eeadfa#page=50
 """
 
-def midi2hz(midi):
-    """
-    Convert MIDI note number(s) to frequency in Hertz (Hz).
-
-    Parameters
-    ----------
-    midi : float or list of float
-        The MIDI note number or a list of MIDI note numbers to convert. 
-        Must be non-negative.
-
-    Returns
-    -------
-    hz : float or list of float
-        The corresponding frequency or list of frequencies in Hz.
-
-    Raises
-    ------
-    ValueError
-        If any MIDI note number is negative.
-    """
-    
-    def convertMidi2Hz(singleMidiNoteNumber):
+def _convert_Midi_to_Hz(singleMidiNoteNumber):
         """
         Convert a single MIDI note number to frequency in Hz.
 
@@ -51,8 +30,31 @@ def midi2hz(midi):
             )
 
         return 440 * (2 ** ((singleMidiNoteNumber - 69) / 12))
+
+def midi2hz(midi):
+    """
+    Convert MIDI note number(s) to frequency in Hertz (Hz).
+
+    Parameters
+    ----------
+    midi : float or list of float
+        The MIDI note number or a list of MIDI note numbers to convert. 
+        Must be non-negative.
+
+    Returns
+    -------
+    hz : float or list of float
+        The corresponding frequency or list of frequencies in Hz.
+
+    Raises
+    ------
+    ValueError
+        If any MIDI note number is negative.
+    """
+    
+    
     
     if isinstance(midi, list):
-        return [convertMidi2Hz(m) for m in midi]
+        return [_convert_Midi_to_Hz(m) for m in midi]
     else:
-        return convertMidi2Hz(midi)
+        return _convert_Midi_to_Hz(midi)
