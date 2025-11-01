deabcf67ea1509d839902588e58ca2910b4d6f1e
Di Wang <diwangislucky@gmail.com>
2025-11-01 23:29:38 +1100

    [dev] added SOM training facilities.

--- amads/pitch/keysom.py
+++ amads/pitch/keysom.py
@@ -25,57 +25,73 @@ Things to watch out for:
 See https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=6e06906ca1ba0bf0ac8f2cb1a929f3be95eeadfa#page=66 for more details
 """
 
-from typing import List, Optional
+import random
+from typing import List, Optional, Tuple
 
 import numpy as np
 
+from amads.algorithms.norm import euclidean_distance
 from amads.core.basics import Concurrence
 from amads.pitch.key import profiles as prof
 
+# TODO: make sure to get rid of some of the random asserts used in development
+# no need to add these restrictions
+
+
+def decay_function(idx: int) -> float:
+    """
+    Global learning rate per iteration.
+
+    """
+    assert idx >= 0
+    return (0.95) ** idx
+
+
+def neighborhood_propagation(
+    coord: Tuple[int], best_match: Tuple[int], idx: int
+) -> float:
+    """
+    Neighborhood propagation update function from BMU.
+
+    This propagation function is designed to decay exponential
+    to the euclidean distance of the node to the BMU on the SOM.
+    """
+    distance = euclidean_distance(coord, best_match, False)
+
+    return (0.5) ** distance
+
+
+def data_selector(list_of_canonicals: List[np.array], idx: int) -> np.array:
+    """
+    Data selector given a list of attribute nmaes and current iteration.
+
+    Current iteration is ignored in this function for now.
+    """
+    return random.choice(list_of_canonicals)[random.randrange(0, 12), :]
+
+
+def update_SOM(SOM: np.array, best_match: Tuple[int], input_data: np.array, idx: int):
+    """
+    updates the SOM on the input data based off of the best matching unit, and the current
+    global training iteration.
+    """
+    dim0, dim1, input_length = SOM.shape
+    if input_length == input_data.shape[0]:
+        raise ValueError(
+            f"self-organizing map has invalid shape {SOM.shape}"
+            "or input is of invalid shape {input_data.shape}"
+        )
+    for i in range(dim0):
+        for j in range(dim1):
+            rate = decay_function(idx) * neighborhood_propagation(
+                (i, j), best_match, idx
+            )
+            SOM[i, j, :] = (1 - rate) * SOM[i, j, :] + rate * input_data
+
 
 def trainprofilesom(
     profile: prof._KeyProfile = prof.KrumhanslKessler,
-    attribute_names: Optional[List[str]] = None,
-):
-    """
-    Trains a self-organizing map based off of the profile and attribute names
-    of the specific pitch profiles we want to train our self-organizing map on.
-
-    TODO: Problems
-    - Should we have a configuration state that we can pass in to make training
-    deterministic?
-
-    Parameters
-    ----------
-    profile: prof.KeyProfile
-        The key profile to use for analysis.
-
-    attribute_names: Optional[List[str]]
-        List of attribute names that denote the particular PitchProfiles
-        within the KeyProfile to compute correlations for.
-        An example of a valid key profile, attribute names combination is
-        something like (prof.vuvan, ["natural_minor", "harmonic_minor"]),
-        which specifies key_cc to compute the crosscorrelation between
-        the pitch-class distribution of the score and both prof.vuvan.natural_minor
-        and prof.vuvan.harmonic_minor.
-        None can be supplied when we want to specify all valid pitch
-        profiles within a given key profile.
-
-    Returns
-    -------
-    Any
-        A self-organizing map (dimensions, type undecided yet)
-    """
-
-    assert 0
-
-# from amads.algorithms.norm import euclidean_distance
-from amads.pitch.key import profiles as prof
-
-
-def trainprofilesom(
-    profile: prof._KeyProfile = prof.KrumhanslKessler,
-    attribute_names: Optional[List[str]] = ["major", "minor"],
+    attribute_names: Optional[List[str]] = ["major_sum", "minor_sum"],
 ):
     """
     Trains a self-organizing map based off of the profile and attribute names
@@ -107,29 +123,33 @@ def trainprofilesom(
     Any
         A self-organizing map (dimensions, type undecided yet)
     """
+    # TODO: fix this later
+    assert attribute_names == ["major_sum", "minor_sum"]
 
-    # the original SOM had dimensions of (12, 36, 24) where
+    list_of_canonicals = [
+        profile[attribute].normalize().as_canonical_matrix()
+        for attribute in attribute_names
+    ]
+
     # 12 is the input length
     # 36 is data width/feature width/something else?
     # 24 is the 12 major and 12 minor keys for the profile data
+    #
+    # SOM indices have been rearranged from matlab version for convenience
+    # in this implementation
+    SOM = np.random.rand(24, 36, 12)
 
-    # potential training regimen:
-    # 2 pseudo-rng generators
-    # initialize SOM numpy array weights with random weights
-    # decay_iterator(idx: int) -> float
-    #   (decay rate specific to iteration idx of training)
-    # neighborhood_propagation(coord: tuple(int), best_match: tuple(int), idx: int) -> float
-    #   (neighborhood propagation decay rate function)
-    # data_selector(profile: prof._KeyProfile, attribute_names: List[str], idx: int) -> List[np.array]
-    #   (selector for data during iteration idx)
+    max_iterations = 36
 
-    # run SOM training algorithm with the previously mentioned variables
-    # one thing to worry about
+    for idx in range(max_iterations):
+        data_vector = data_selector(list_of_canonicals, idx)
+        # tensor multiplication (need to figure out index)
+        application = SOM @ data_vector
+        # figure out best match
+        best_match = np.unravel_index(np.argmax(application))
+        update_SOM(SOM=SOM, best_match=best_match, input_data=data_vector, idx=idx)
 
-    # decay rates are mutually independent, also update function seems to
-    # be suspiciously similar to bernoulli random variable expectation
-
-    assert 0
+    return SOM
 
 
 def keysom(
