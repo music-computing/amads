9686cb9b58137bffef7d069b0063e3a9b4dd199d
Mark Gotham <19486660+MarkGotham@users.noreply.github.com>
2026-02-17 16:42:54 +0000

    Add `set_reader_warning_level` for good measure

--- tests/test_readscore.py
+++ tests/test_readscore.py
@@ -4,10 +4,11 @@ Basic start on testing readscore.
 Important business first: score from URL ;)
 """
 
-from amads.io.readscore import read_score
+from amads.io.readscore import read_score, set_reader_warning_level
 
 
 def test_pitch_comparison():
+    set_reader_warning_level("none")
     test_path = (
         "https://github.com/MarkGotham/species/raw/refs/heads/main/1x1/005.mxl"
     )
