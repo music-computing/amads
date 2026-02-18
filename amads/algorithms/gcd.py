6cc8207692bb14f4bebaecb00705caac20eb00a9
Mark Gotham <19486660+MarkGotham@users.noreply.github.com>
2026-02-18 14:37:17 +0000

    refactor by type and pair/longer

--- amads/algorithms/gcd.py
+++ amads/algorithms/gcd.py
@@ -1,8 +1,14 @@
 """
 Basic, local calculations of the greatest common divisor (GCD)
 at a central point in the code base for wide applicability.
+Parameter-specific adaptations are in the relevant place (e.g., `amads.time.meter`).
 
-Rhythm-specific modifications are stored there.
+The module is organised in a kind of logical quadrant for
+data type (e.g., integer vs fraction)
+and
+number (a single pair vs many pairwise calculations).
+
+<small>**Author**: Mark Gotham</small>
 """
 
 __author__ = "Mark Gotham"
@@ -10,11 +16,25 @@ __author__ = "Mark Gotham"
 
 from fractions import Fraction
 
+# -----------------------------------------------------------------------------
 
-def gcd_pair(a: int, b: int) -> int:
+# Direct operations on pair of values, known to be integer, float, fraction.
+
+
+def integer_gcd_pair(a: int, b: int) -> int:
     """
     Calculates the greatest common divisor (GCD) of two integers using the
     Euclidean algorithm.
+
+    >>> integer_gcd_pair(0, 2)
+    2
+
+    >>> integer_gcd_pair(15, 16)
+    1
+
+    >>> integer_gcd_pair(8, 16)
+    8
+
     """
     while b:
         a, b = b, a % b
@@ -23,8 +43,8 @@ def gcd_pair(a: int, b: int) -> int:
 
 def float_gcd_pair(a: float, b: float = 1.0, rtol=1e-05, atol=1e-08) -> float:
     """
-    Calculate the greatest common divisor (GCD) for values a and b given the specified
-    relative and absolute tolerance (rtol and atol).
+    Calculate approximate greatest common divisor (GCD) for values a and b given the specified
+    relative and absolute tolerance (`rtol` and `atol`).
     With thanks to Euclid,
     `fractions.gcd`, and
     [stackexchange](https://stackoverflow.com/questions/45323619/).
@@ -82,8 +102,17 @@ def float_gcd_pair(a: float, b: float = 1.0, rtol=1e-05, atol=1e-08) -> float:
 
 
 def local_lcm_pair(a: int, b: int) -> int:
-    """Local implementation of the Lowest Common Multiple (LCM)."""
-    return a * b // gcd_pair(a, b)
+    """
+    Local implementation of the Lowest Common Multiple (LCM).
+
+    >>> local_lcm_pair(8, 16)
+    16
+
+    >>> local_lcm_pair(2, 3)
+    6
+
+    """
+    return a * b // integer_gcd_pair(a, b)
 
 
 def fraction_gcd_pair(x: Fraction, y: Fraction) -> Fraction:
@@ -106,15 +135,92 @@ def fraction_gcd_pair(x: Fraction, y: Fraction) -> Fraction:
 
     """
     return Fraction(
-        gcd_pair(x.numerator, y.numerator),
+        integer_gcd_pair(x.numerator, y.numerator),
         local_lcm_pair(x.denominator, y.denominator),
     )
 
 
-def fraction_gcd_list(fraction_list: list[Fraction]):
+# -----------------------------------------------------------------------------
+
+# Combined operations on iterable (list, tuple, ... ) of values.
+
+
+def calculate_gcd(numbers: list):
     """
-    Iterate GCD calculation over a list of Fractions.
-    See `fraction_gcd`
+    Compute GCD.
+    Wrapper function when you don't know whether the type of the numeric data.
+    If the value type is known (integer, fractions, float),
+    use the more specific `{type}_gcd` function.
+
+    Here, the logic is that integers and fractions are lossless, but floats are not
+    so process the former first and fractions
+    and then the latter as needed.
+
+    >>> calculate_gcd([1, 2])
+    Fraction(1, 1)
+
+    >>> calculate_gcd([1, Fraction(1, 2), 2])
+    Fraction(1, 2)
+
+    >>> calculate_gcd([0, 1/2])
+    0.5
+
+    >>> calculate_gcd([0, Fraction(1, 2), 1/2])
+    Fraction(1, 2)
+
+    >>> gcd = calculate_gcd([0, Fraction(1, 2), 4/12])
+    >>> round(gcd, 3)
+    0.167
+
+    """
+    floats = [num for num in numbers if isinstance(num, float)]
+    ints_fractions = [num for num in numbers if not isinstance(num, float)]
+
+    if ints_fractions:
+        gcd = Fraction(ints_fractions[0])
+        for num in ints_fractions[1:]:
+            gcd = fraction_gcd_pair(Fraction(num), gcd)
+    else:
+        gcd = floats.pop(0)  # seed with first float
+
+    for f in floats:
+        gcd = float_gcd_pair(f, gcd)
+
+    return gcd
+
+
+def integer_gcd(integers: list[int]) -> int:
+    """
+    Compute GCD where the elements are known/asserted to be integers.
+    See `integer_gcd_pair`.
+
+    Returns
+    -------
+    tuple
+        The GCD of all elements in the list of integers.
+
+    Examples
+    --------
+    >>> integer_gcd([0, 2, 4])
+    2
+
+    >>> integer_gcd([0, 15, 16])
+    1
+
+    >>> integer_gcd([0, 8, 16])
+    8
+
+    """
+    gcd = integers[0]
+    for i in range(1, len(integers)):
+        gcd = integer_gcd_pair(gcd, integers[i])
+    return gcd
+
+
+def fraction_gcd(fractions: list[Fraction]) -> Fraction:
+    """
+    Compute GCD where all elements are known/asserted to be Fractions.
+    See `fraction_gcd_pair`
 
     Returns
     -------
@@ -123,11 +229,37 @@ def fraction_gcd_list(fraction_list: list[Fraction]):
 
     Examples
     --------
-    >>> fraction_gcd_list([Fraction(1, 2), Fraction(2, 3), Fraction(5, 12)])
+    >>> fraction_gcd([Fraction(1, 2), Fraction(2, 3), Fraction(5, 12)])
     Fraction(1, 12)
 
     """
-    current_gcd = fraction_list[0]
-    for i in range(1, len(fraction_list)):
-        current_gcd = fraction_gcd_pair(current_gcd, fraction_list[i])
-    return current_gcd
+    gcd = fractions[0]
+    for i in range(1, len(fractions)):
+        gcd = fraction_gcd_pair(gcd, fractions[i])
+    return gcd
+
+
+def float_gcd(floats: list[float], rtol=1e-05, atol=1e-08) -> float:
+    """
+    Calculate GCD for values given the specified
+    relative and absolute tolerance (rtol and atol).
+
+    If the values are known to be
+    integers use `integer_gcd`,
+    fractions
+    and if the type is not known, use `calculate_gcd`.
+
+    Parameters
+    ----------
+    floats: list[float]
+        Any float value.
+    rtol
+        the relative tolerance
+    atol
+        the absolute tolerance
+
+    """
+    gcd = floats[0]
+    for i in range(1, len(floats)):
+        gcd = float_gcd_pair(gcd, floats[i], rtol=rtol, atol=atol)
+    return gcd
