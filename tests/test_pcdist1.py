5b5f9f67959d1128fdb9f158b94233b992446ee8
Di Wang <diwangislucky@gmail.com>
2026-08-27 12:49:43 +1000

    [test] added crosstesting input and matlab crosstesting utilities

--- tests/test_pcdist1.py
+++ tests/test_pcdist1.py
@@ -2,8 +2,14 @@
 Test suite for pcdist1
 """
 
+import math
+
 from amads.core.basics import Score
 from amads.pitch.pcdist1 import pitch_class_distribution_1
+from tests.matlab_crosstesting_utils import (
+    load_json_results,
+    matlab_sarabande_test_score,
+)
 
 
 def test_empty_melody():
@@ -11,3 +17,23 @@ def test_empty_melody():
     val = pitch_class_distribution_1(score)
     test = [0] * 12
     assert val.data == test
+
+
+def test_sarabande():
+    json_data = load_json_results()
+    # This desired data result is obtained from matlab testing
+    desired_data = json_data["pcdist1"]
+
+    pcd = pitch_class_distribution_1(
+        matlab_sarabande_test_score, weighted=True, miditoolbox_compatible=True
+    )
+
+    assert all(
+        math.isclose(desired, pcd_datum)
+        for desired, pcd_datum in zip(desired_data, pcd.data)
+    )
+
+
+if __name__ == "__main__":
+    test_empty_melody()
+    test_sarabande()
